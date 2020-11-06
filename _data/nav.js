b95e5cba74dc00802fde146b05e56f3566b00138
espern <loic.espern@gmail.com>
2020-11-06 20:51:42 +0100

    move generated pages on 'docs' subfolder

--- _data/nav.js
+++
@@ -1,9 +0,0 @@
-exports.data = [
-  {label: "Accueil", url: "/",},
-  {label: "Historique", url: "/pages/dommartin/historique",},
-  {label: "Chapelle", url: "/pages/dommartin/chapelle-niai-nion",},
-  {label: "Croix", url: "/pages/dommartin/croix-de-chemin",},
-  {label: "Eglise", url: "/pages/dommartin/eglise-saint-martin",},
-  {label: "Vivre à Dommartin", url: "/pages/dommartin/vivre-a-dommartin",},
-  {label: "Contact", url: "/pages/contact",},
-];
---
+++ pages/_data/nav.js
@@ -0,0 +1,9 @@
+exports.data = [
+  {label: "Accueil", url: "/",},
+  {label: "Historique", url: "/dommartin/historique",},
+  {label: "Chapelle", url: "/dommartin/chapelle-niai-nion",},
+  {label: "Croix", url: "/dommartin/croix-de-chemin",},
+  {label: "Eglise", url: "/dommartin/eglise-saint-martin",},
+  {label: "Vivre à Dommartin", url: "/dommartin/vivre-a-dommartin",},
+  {label: "Contact", url: "/contact",},
+];
